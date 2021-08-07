dc4edd7833faa267fc9015049ea69bc50fe2b382
lily516liu <lily516liu@gmail.com>
2021-08-07 16:19:48 +0800

    修改註解

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,6 +1,8 @@
 const axios = require("axios"); //引用axios
 const moment = require("moment"); //引用moment套件
+// moment().format("YYYYMMDD") moment用法
 
+// axios文件用法
 axios
   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
     params: {
