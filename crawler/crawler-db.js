bba586895f6caa7ec0b9ac3c8803c30cdb46923c
lily516liu <lily516liu@gmail.com>
2021-08-14 10:26:41 +0800

    add crawler-db.js

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -2,60 +2,112 @@ const axios = require("axios"); //引用axios
 const moment = require("moment"); //引用moment套件
 const fs = require("fs");
 const mysql = require("mysql"); //連線到mysql
+const { resolve } = require("path");
+const { rejects } = require("assert");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_host,
-  user: process.env.DB_user,
-  port: process.env.DB_port,
-  password: process.env.DB_password,
-  database: process.env.DB_database,
-});
+//再一次用async await呼叫
+async function getData() {
+  try {
+    // 函式一 先讀stock.txt
+    let stockCode = await new Promise((resolve, reject) => {
+      fs.readFile("stock.txt", "utf8", function (err, stockCode) {
+        //把非同步函式搬進來
+        if (err) {
+          reject(err);
+        } else {
+          resolve(stockCode);
+        }
+      });
+    });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("資料庫連不上");
+    // 連線到mysql
+    const connection = mysql.createConnection({
+      host: process.env.DB_host,
+      user: process.env.DB_user,
+      port: process.env.DB_port,
+      password: process.env.DB_password,
+      database: process.env.DB_database,
+    });
+
+    connection.connect((err) => {
+      if (err) {
+        console.error("資料庫連不上");
+      }
+    });
+
+    let dbresult = await new Promise(function(resolve,rejects){
+      // 連線後再用sql語法撈資料
+      connection.query(
+        `SELECT * FROM stock WHERE stock_id= ?`,
+        [stockCode],
+        function (error, results, fields) {
+          if (error) {
+          rejects(error);
+          }
+          resolve(results); //資料庫中的代碼資料 
+        }
+      )
+    });
+    console.log(results);
+
+    // // 連線後再用sql語法撈資料
+    // connection.query(
+    //   `SELECT * FROM stock WHERE stock_id= ?`,
+    //   [stockCode],
+    //   function (error, results, fields) {
+    //     if (error) throw error;
+    //     // console.log(results); //資料庫中的代碼資料 
+    //     if (results.length > 0) {
+    //       new Promise(function (resolve, rejects) {
+    //         //函式二 再用axios連到對方的json檔案
+    //         async function twseData(stockCode) {
+    //           let twse = await axios.get(
+    //             "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+    //             {
+    //               //用return 再把then丟出去
+    //               params: {
+    //                 response: "json",
+    //                 date: moment().format("YYYYMMDD"),
+    //                 stockNo: stockCode,
+    //               },
+    //             }
+    //           );
+    //           return twse;
+    //         }
+    //         let result = twseData(stockCode);
+    //         resolve(result.data);
+    //         rejects("錯誤");
+    //       });
+    //     } else {
+    //       console.log("資料不在範圍內");
+    //     }
+    //   }
+    // );
+
+    // 資料庫連線關閉
+    connection.end();
+
+    
+  } catch (err) {
+    console.log(err);
   }
-});
+}
+getData();
 
-// 不關閉連線，認為程式一直在執行
-connection.end();
 
+
+
+// 讀 stock.txt 把股票代碼讀進來 OK
+// 去資料庫的 stock 表格查看看，這個代碼是不是在我們的服務範圍內
+// 如果是，才去證交所抓資料
+// 抓回來的資料存到資料庫的 stock_price 表格裡去
 // 先讀 stock.txt > 再用axios > 再console.log資料
-// 把函式都先拉出來寫比較美
-// 函式一
-// function resultCode() {
-//   return new Promise((resolve, reject) => {
-//     fs.readFile("stock.txt", "utf8", function (err, stockCode) {
-//       //把非同步函式搬進來
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(stockCode);
-//       }
-//     });
-//   });
-// }
-// // 函式二
-// function resultData(stockCode) {
-//   return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-//     //用return 再把then丟出去
-//     params: {
-//       response: "json",
-//       date: moment().format("YYYYMMDD"),
-//       stockNo: stockCode,
-//     },
-//   });
-// }
 
-// //再一次用async await呼叫
-// async function getData() {
-//   try {
-//     let stockCode = await resultCode();
-//     let result = await resultData(stockCode);
-//     console.log(result.data);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// getData();
+
+
+
+
+
+
+
