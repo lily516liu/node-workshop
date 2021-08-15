0179f6c53242781533cebcf2bb57ce6e2c0044ec
lily516liu <lily516liu@gmail.com>
2021-08-15 11:45:05 +0800

    add sever.js

--- simple-express/sever.js
+++ simple-express/sever.js
@@ -4,7 +4,7 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
-//要造訪頁面才會經過流程
+//要造訪頁面才會經過流程 要有請求才會觸發
 app.use((request,response,next)=>{
     let time = new Date();
     console.log(`我是第一名 在 ${time.toISOString()}`);
