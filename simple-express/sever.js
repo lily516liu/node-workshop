b7288280be1a02a11ba9e2d3c9e3afba9a8761fb
lily516liu <lily516liu@gmail.com>
2021-08-15 11:40:54 +0800

    add sever.js

--- simple-express/sever.js
+++ simple-express/sever.js
@@ -1,7 +1,21 @@
+const { response } = require("express");
 const express = require("express");
 
 // 利用 express 建立了一個 express application
 let app = express();
+
+//要造訪頁面才會經過流程
+app.use((request,response,next)=>{
+    let time = new Date();
+    console.log(`我是第一名 在 ${time.toISOString()}`);
+    next();
+});
+
+app.use((request,response,next)=>{
+    let time = new Date();
+    console.log(`我是第二名 在 ${time.toISOString()}`);
+    next();
+});
 
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
@@ -10,7 +24,7 @@ app.get("/", function (request, response, next) {
 });
 
 app.get("/about", function (request, response, next) {
-    response.send("About");
+    response.send("About me");
   });
 
 app.listen(3000, function () {
