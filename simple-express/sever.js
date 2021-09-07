614f0a33611c63b4cafc6aace6ce76d6f90d578f
lily516liu <lily516liu@gmail.com>
2021-09-07 12:33:58 +0800

    新增simple express筆記

--- simple-express/sever.js
+++ simple-express/sever.js
@@ -30,8 +30,30 @@ app.get("/", function (request, response, next) {
   response.send("Hello nodemon");
 });
 
+
+// 也可以寫判斷式進到next錯誤訊息
+// let isLogin = false;
+// if(isLogin){
+//   next();
+// }else{
+//   next({
+//     status:401,
+//     message:"沒登入",
+//   });
+// }
+
 app.get("/about", function (request, response, next) {
-  response.send("About me");
+  // response.send("About me");
+  // next()沒帶參數，正常前往下一個
+  // next()中帶參數，呼叫express有錯誤，用捕捉錯誤的middleware去接
+  // next("去B啊");
+  console.log("我是about");
+  // 將next中放入錯誤訊息物件 統一丟給特殊middleware處理
+  next({
+    code:"1001",
+    status:500,
+    message:"測試錯誤"
+  });
 });
 
 app.get("/stock", async (req,res,next)=>{
@@ -56,6 +78,12 @@ app.use((req,res,next)=>{
   res.status(404).send("Sorry 查無資料");
 })
 
+// about中有next("去B啊")有參數->呼叫捕捉錯誤的middleware 因此跑到這裡
+// ** 超級特殊的middleware有四個參數 用來捕捉錯誤的 通常放在最下面
+app.use((err,req,res,next)=>{
+  console.error(err); // err接的是next()中的東西
+  res.status(err.status).json(err.message);
+});
 
 app.listen(3001, async function () {
   // 因為用createPool 所以不用預先建立連線
